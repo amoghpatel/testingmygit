7db38cf95f6d05dbf16d8c5180f9ed7bfe318471
Amogh Patel <amoghpatel4u@gmail.com>
2015-09-23 12:15:48 -0500

    test.pp

---
+++ test.pp
@@ -0,0 +1,12 @@
+# == Define: xxxxxxxxx::xxxxxxx
+#
+#    Txxxxxxxxxxxxxxxxxxxxxxxxx,
+#    xxxxxxxxxxxxxxxxxxxxxxxxxxx
+#    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+#    dxxxxxxxxxxxxxxxxxxxx.
+#
+#
+define xxxxxxxxxxxxxxxxxxxxxxx) {
+
+  # xxxxxxxxxxxxxxxxxxxxxxxx
+  include xxxxxxxxxxxxxxxxxxx
